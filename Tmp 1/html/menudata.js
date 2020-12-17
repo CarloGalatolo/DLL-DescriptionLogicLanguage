9ff813def6decbb273ca74799ec63a202749db49
Salvatore Signorino <salvatore.signorino1701@gmail.com>
2020-12-17 17:13:46 +0100

    Carlo, scelgo te!

--- Tmp 1/html/menudata.js
+++ Tmp 1/html/menudata.js
@@ -24,6 +24,7 @@
 */
 var menudata={children:[
 {text:"Main Page",url:"index.html"},
+{text:"Logica Descrittiva",url:"description_logic.html"},
 {text:"Caratteristiche del Linguaggio", children:[
 {text:"Decomposizione",  url:"decomposition.html"},
 {text:"Specifiche tecniche dell'interprete", url:"interpreter.html"},
